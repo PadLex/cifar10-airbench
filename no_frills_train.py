656883e15435a5f1db7a9cb1c1ab40e3bd6e1d55
Baraq Lipshitz <baraqlip@gmail.com>
2026-01-03 22:50:59 +0100

    update torch again to latest, saving model checkpoint.

--- no_frills_train.py
+++ no_frills_train.py
@@ -1,6 +1,10 @@
 import torch
 from airbench import infer, evaluate, CifarLoader
 from airbench.lib_nofrills import train94, make_net94
+from pathlib import Path
+
+CHECKPOINT_FOLDER = Path("/work/scratch/deep-learning/checkpoints/")
+CHECKPOINT_NAME = "test"
 
 loader = CifarLoader(
     'cifar10',
@@ -11,6 +15,11 @@ loader = CifarLoader(
 )
 
 net = train94(label_smoothing=0)
+
+# save
+CHECKPOINT_FOLDER.mkdir(parents=True, exist_ok=True)
+torch.save(net.state_dict(), CHECKPOINT_FOLDER / f"{CHECKPOINT_NAME}.pt")
+
 logits = infer(net, loader)
 conf = logits.log_softmax(1).amax(1) # confidence
-# print(conf)
+print(conf)
